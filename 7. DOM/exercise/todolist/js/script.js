fa7520a3dc5c08874f2268eea505e0bd91f1873e
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2024-05-13 19:13:20 +0330

    dom

---
+++ 7. DOM/exercise/todolist/js/script.js
@@ -0,0 +1,41 @@
+﻿let $ = document;
+let inputElem = $.querySelector("input");
+let addTodoForm = $.querySelector(".add");
+let todoUlElem = $.querySelector(".todos");
+
+function addNewTodo(newTodoValue) {
+  let newTodoLi = $.createElement("li");
+  newTodoLi.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+
+  let newTodoTitleSpan = $.createElement("span");
+  newTodoTitleSpan.innerHTML = newTodoValue;
+
+  let newTodoTrash = $.createElement("i");
+  newTodoTrash.className = "fa fa-trash-o delete";
+
+  newTodoTrash.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+
+  newTodoLi.append(newTodoTitleSpan, newTodoTrash);
+
+  todoUlElem.append(newTodoLi);
+
+  console.log(newTodoLi);
+}
+
+addTodoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+inputElem.addEventListener("keydown", function (event) {
+  let newTodoValue = event.target.value.trim();
+
+  if (event.keyCode === 13) {
+    if (newTodoValue) {
+      inputElem.value = "";
+      addNewTodo(newTodoValue);
+    }
+  }
+});
