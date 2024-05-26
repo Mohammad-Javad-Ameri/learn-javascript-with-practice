7a1b4962757c774785b5f3a98c51fd2db13e5b55
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2024-05-26 19:09:19 +0330

    add some event

--- 8. events/exercise/localtodo/js/app.js
+++ 8. events/exercise/localtodo/js/app.js
@@ -8,24 +8,19 @@ let todosArray = [];
 
 function addNewTodo() {
   let newTodoTitle = inputElem.value;
-
   let newTodoObj = {
     id: todosArray.length + 1,
     title: newTodoTitle,
     complete: false,
   };
-
   inputElem.value = "";
-
   todosArray.push(newTodoObj);
   setLocalStorage(todosArray);
   todosGenerator(todosArray);
-
-  inputElem.blur();
 }
-
-function setLocalStorage(todosList) {
-  localStorage.setItem("todos", JSON.stringify(todosList));
+console.log(todosArray);
+function setLocalStorage(todosArray) {
+  localStorage.setItem("todos", JSON.stringify(todosArray));
 }
 
 function todosGenerator(todosList) {
@@ -66,11 +61,23 @@ function todosGenerator(todosList) {
   });
 }
 
+function removeTodo(todoId) {
+  let localStorageTodos = JSON.parse(localStorage.getItem("todos"));
+  todosArray = localStorageTodos;
+
+  let mainTodoIndex = todosArray.findIndex(function (todo) {
+    return todo.id === todoId;
+  });
+  todosArray.splice(mainTodoIndex, 1);
+  setLocalStorage(todosArray);
+  todosGenerator(todosArray);
+}
+
 function editTodo(todoId) {
   let localStorageTodos = JSON.parse(localStorage.getItem("todos"));
 
   todosArray = localStorageTodos;
-
+  console.log(todosArray);
   todosArray.forEach(function (todo) {
     if (todo.id === todoId) {
       todo.complete = !todo.complete;
@@ -81,43 +88,25 @@ function editTodo(todoId) {
   todosGenerator(todosArray);
 }
 
-function removeTodo(todoId) {
-  let localStorageTodos = JSON.parse(localStorage.getItem("todos"));
-
-  todosArray = localStorageTodos;
-
-  let mainTodoIndex = todosArray.findIndex(function (todo) {
-    return todo.id === todoId;
-  });
-
-  todosArray.splice(mainTodoIndex, 1);
-
-  setLocalStorage(todosArray);
-  todosGenerator(todosArray);
-}
-
 function getLocalStorage() {
   let localStorageTodos = JSON.parse(localStorage.getItem("todos"));
-
   if (localStorageTodos) {
     todosArray = localStorageTodos;
   } else {
     todosArray = [];
   }
-
   todosGenerator(todosArray);
 }
 
-function clearTodos() {
+function clearTodo() {
   todosArray = [];
   todosGenerator(todosArray);
-  // localStorage.clear()
   localStorage.removeItem("todos");
 }
 
-window.addEventListener("load", getLocalStorage);
 addButton.addEventListener("click", addNewTodo);
-clearButton.addEventListener("click", clearTodos);
+
+clearButton.addEventListener("click", clearTodo);
 inputElem.addEventListener("keydown", function (event) {
   if (event.code === "Enter") {
     addNewTodo();
--- 8. events/exercise/range/js/script.js
+++ 8. events/exercise/range/js/script.js
@@ -1,7 +1,7 @@
-const rangeInput = document.querySelector('input')
-const container = document.querySelector('.container')
+const rangeInput = document.querySelector("input");
+const container = document.querySelector(".container");
 
-
-rangeInput.addEventListener('change', function (event) {
-    container.style.filter = 'brightness(' + event.target.value + '%)'
-})
+rangeInput.addEventListener("change", function (event) {
+  console.log(event.target.value);
+  container.style.filter = `brightness(${event.target.value}%)`;
+});
