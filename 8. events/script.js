ed886be095f6b2e3051e05c6d92626d250b16144
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2024-05-30 19:07:17 +0330

    start the expert section

--- 8. events/script.js
+++ 8. events/script.js
@@ -1181,3 +1181,50 @@ window.addEventListener("scroll", function () {
 
   console.log(scrollPercentRounded);
 });
+
+// window
+
+// history
+
+// location
+
+//Dynamic Route
+
+let userInfo = document.querySelector("h1");
+
+let users = [
+  { id: 1, name: "Amin", age: 21 },
+  { id: 2, name: "Amir", age: 23 },
+  { id: 3, name: "Ali", age: 24 },
+  { id: 4, name: "Hasan", age: 19 },
+  { id: 5, name: "Babak", age: 29 },
+  { id: 6, name: "Reza", age: 32 },
+];
+
+let locationSearch = location.search;
+
+// Bad Way
+// location.search.slice(1).split('=')[1]
+
+// Good Way
+let locationSearchParams = new URLSearchParams(locationSearch);
+let userIDParam = locationSearchParams.get("id");
+
+let mainUser = users.find(function (user) {
+  return user.id == userIDParam;
+});
+
+if (mainUser) {
+  userInfo.innerHTML = "Name: " + mainUser.name + " | Age: " + mainUser.age;
+} else {
+  userInfo.innerHTML = "User Is Not Defined :(";
+}
+
+console.log(mainUser);
+
+// access dom
+let textElem = document.getElementsByClassName("text")[0];
+
+title.style.color = "green";
+
+console.log(console);
